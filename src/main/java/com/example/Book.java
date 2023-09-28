2aa1850ce92b4c6e66d2fda20e9f2440e32d939f
Hogan <hoganwong88@gmail.com>
2023-09-28 21:09:01 +0800

    edits

--- src/main/java/com/example/Book.java
+++ src/main/java/com/example/Book.java
@@ -7,7 +7,30 @@ public class Book {
     private boolean checkedOut;
 
     public Book(String title, String author, String ISBN) {
-        // TODO: Initialize the attributes
+        this.title=title;
+        this.author=author;
+        this.ISBN=ISBN;
+        this.checkedOut=checkedOut;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public String getISBN() {
+        return this.ISBN;
+    }
+
+    public boolean isCheckedOut() {
+        return this.checkedOut;
+    }
+
+    public void setTitle() {
+        this.title=title;
     }
 
     // TODO: Implement getters, setters, and any other methods you find necessary
